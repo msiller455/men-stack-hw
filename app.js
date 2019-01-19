095f3a77edf411c4281afb2ee94b55588c2175ca
Michael <msiller455@gmail.com>
2019-01-19 14:53:28 -0800

    set up routers

--- app.js
+++ app.js
@@ -1,17 +1,18 @@
 require('./db/db');
-
 const express = require('express');
 const app = express();
-const methodOverride = require('method-override');
 
+
+const methodOverride = require('method-override');
 const bodyParser = require('body-parser');
 
+const beersRouter = require('./routes/beers');
 
 app.use(methodOverride('_method'));
 app.use(bodyParser.urlencoded({extended: true}));
+app.use(express.static('public'));
 
-
-
+app.use('/beers', beersRouter);
 
 app.listen(8000, () => {
     console.log('Server is listening on port 8000');
---
+++ routes/beers.js
@@ -0,0 +1,39 @@
+const express = require('express');
+const router = express.Router();
+
+//Index Route
+router.get('/', (req, res) => {
+
+});
+
+//New Route
+router.get('/new', (req, res) => {
+
+});
+
+//Create Route
+router.post('/', (req, res) => {
+
+});
+
+//Edit Route
+router.get('/:id/edit', (req, res) => {
+
+});
+
+//Update Route
+router.put('/:id', (req, res) => {
+
+});
+
+//Show Route
+router.get('/:id', (req, res) => {
+
+});
+
+//Delete Route
+router.get('/:id', (req, res) => {
+
+});
+
+module.exports = router;
